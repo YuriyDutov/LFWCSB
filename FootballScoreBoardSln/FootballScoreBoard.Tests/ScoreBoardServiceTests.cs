6c699bf1adda32f0848e20be15726dbad6f837ac
yuriy_dutov <yuradtv@gmail.com>
2023-07-12 00:06:47 +0300

    Implementation of  "Get" methods for the Service and Repository

--- FootballScoreBoardSln/FootballScoreBoard.Tests/ScoreBoardServiceTests.cs
+++ FootballScoreBoardSln/FootballScoreBoard.Tests/ScoreBoardServiceTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using FluentAssertions;
 using Xunit;
 
@@ -10,8 +11,9 @@ namespace FootballScoreBoard.Tests
         [Fact]
         public void GetCurrentFootballMatches_GetsCurrentFootballMatches_ReturnListOfFootballMatchesOrderedByTotalScoreAndInProgressIsTrue()
         { 
-            ////Arrange
-            var scoreBoardService = new ScoreBoardService();
+            //Arrange
+            var scoreBoardRepository = new ScoreBoardRepository();
+            var scoreBoardService = new ScoreBoardService(scoreBoardRepository);
 
             //Act
             List<FootballMatch> result = scoreBoardService.GetCurrentFootballMatches();
@@ -67,9 +69,81 @@ namespace FootballScoreBoard.Tests
 
     public class ScoreBoardService
     {
+        private readonly IScoreBoardRepository _repository;
+
+        public ScoreBoardService(IScoreBoardRepository repository)
+        {
+            _repository = repository;
+        }
+
         public List<FootballMatch> GetCurrentFootballMatches()
         {
-            throw new NotImplementedException();
+            return _repository.Get().Where(x => x.InProgress == true).OrderByDescending(m => m.TotalScore).ToList();
+        }
+    }
+
+    public interface IScoreBoardRepository
+    {
+        List<FootballMatch> Get();
+    }
+
+    public class ScoreBoardRepository: IScoreBoardRepository
+    {
+        public List<FootballMatch> Get()
+        {
+            return new List<FootballMatch>
+            {
+                new FootballMatch
+                {
+                    Id = 1,
+                    HomeTeam = "Mexico",
+                    AwayTeam = "Canada",
+                    HomeTeamScore = 0,
+                    AwayTeamScore = 5,
+                    InProgress = true,
+                    TotalScore = 5
+                },
+                new FootballMatch
+                {
+                    Id = 2,
+                    HomeTeam = "Spain",
+                    AwayTeam = "Brazil",
+                    HomeTeamScore = 10,
+                    AwayTeamScore = 2,
+                    InProgress = true,
+                    TotalScore = 12
+                },
+                new FootballMatch
+                {
+                    Id = 3,
+                    HomeTeam = "Germany",
+                    AwayTeam = "France",
+                    HomeTeamScore = 2,
+                    AwayTeamScore = 2,
+                    InProgress = true,
+                    TotalScore = 4
+                },
+                new FootballMatch
+                {
+                    Id = 4,
+                    HomeTeam = "Uruguay",
+                    AwayTeam = "Italy",
+                    HomeTeamScore = 6,
+                    AwayTeamScore = 6,
+                    InProgress = true,
+                    TotalScore = 12
+                },
+                new FootballMatch
+                {
+                    Id = 5,
+                    HomeTeam = "Argentina",
+                    AwayTeam = "Australia",
+                    HomeTeamScore = 3,
+                    AwayTeamScore = 1,
+                    InProgress = true,
+                    TotalScore = 4
+                }
+            };
         }
     }
 }
