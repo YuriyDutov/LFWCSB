3b3804f473dd6c1e36962110eba0060ee4ce5250
yuriy_dutov <yuradtv@gmail.com>
2023-07-11 23:14:52 +0300

    Added Test methods to the test class

--- FootballScoreBoardSln/FootballScoreBoard.Tests/ScoreBoardServiceTests.cs
+++ FootballScoreBoardSln/FootballScoreBoard.Tests/ScoreBoardServiceTests.cs
@@ -5,5 +5,45 @@ namespace FootballScoreBoard.Tests
 {
     public class ScoreBoardServiceTests
     {
+        [Fact]
+        public void GetCurrentFootballMatches_GetsCurrentFootballMatches_ReturnListOfFootballMatchesOrderedByTotalScoreAndInProgressIsTrue()
+        { 
+            ////Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [Fact]
+        public void StartFootballMatch_CreatesFootballMatchEntityWithInProgressIsTrue_ReturnTrue()
+        { 
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [Fact]
+        public void FinishFootballMatch_UpdatesFootballMatchEntityWithInProgressIsFalse_ReturnTrue()
+        { 
+            //Arrange
+
+            //Act
+
+            //Assert
+
+        }
+
+        [Fact]
+        public void UpdateScore_UpdatesFootballMatchEntityScore_ReturnTrue()
+        { 
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
     }
 }
