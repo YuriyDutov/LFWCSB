cdf1caf0fcde58bdea6ce67790cda4c821057742
yuriy_dutov <yuradtv@gmail.com>
2023-07-12 01:34:30 +0300

    Create Unit Tests for the Service methods

--- FootballScoreBoardSln/FootballScoreBoard.Tests/ScoreBoardServiceTests.cs
+++ FootballScoreBoardSln/FootballScoreBoard.Tests/ScoreBoardServiceTests.cs
@@ -31,20 +31,43 @@ namespace FootballScoreBoard.Tests
         public void StartFootballMatch_CreatesFootballMatchEntityWithInProgressIsTrue_ReturnTrue()
         { 
             //Arrange
+            var scoreBoardRepository = new ScoreBoardRepository();
+            var scoreBoardService = new ScoreBoardService(scoreBoardRepository);
+            var countOfMatches = scoreBoardService.GetCurrentFootballMatches().Count;
+            var id = scoreBoardService.GetCurrentFootballMatches().Max(m => m.Id) + 1;
+            var footballMatch = new FootballMatch
+            {
+                HomeTeam = "England",
+                AwayTeam = "Wales",
+                Id = id,
+                InProgress = true
+            };
 
             //Act
+            var result = scoreBoardService.StartNewMatch(footballMatch);
+            var expectedCountOfMatches = scoreBoardService.GetCurrentFootballMatches().Count;
 
             //Assert
+            result.Should().Be(true);
+            expectedCountOfMatches.Should().BeGreaterThan(countOfMatches);
         }
 
         [Fact]
         public void FinishFootballMatch_UpdatesFootballMatchEntityWithInProgressIsFalse_ReturnTrue()
         { 
             //Arrange
+            var scoreBoardRepository = new ScoreBoardRepository();
+            var scoreBoardService = new ScoreBoardService(scoreBoardRepository);
+            var countOfMatches = scoreBoardService.GetCurrentFootballMatches().Count;
+            var id = 1;
 
             //Act
+            var result = scoreBoardService.EndCurrentMatch(id);
+            var expectedCountOfMatches = scoreBoardService.GetCurrentFootballMatches().Count;
 
             //Assert
+            result.Should().Be(true);
+            expectedCountOfMatches.Should().BeLessThan(countOfMatches);
 
         }
 
@@ -52,10 +75,26 @@ namespace FootballScoreBoard.Tests
         public void UpdateScore_UpdatesFootballMatchEntityScore_ReturnTrue()
         { 
             //Arrange
+            var scoreBoardRepository = new ScoreBoardRepository();
+            var scoreBoardService = new ScoreBoardService(scoreBoardRepository);
+            var match = new FootballMatch
+            {
+                Id = 5,
+                HomeTeam = "Argentina",
+                AwayTeam = "Australia",
+                HomeTeamScore = 3,
+                AwayTeamScore = 1,
+                InProgress = true,
+                TotalScore = 4
+            };
 
             //Act
+            var result = scoreBoardService.UpdateMatchScore(match.Id, match.HomeTeamScore, match.AwayTeamScore);
+            var expectedTotalScore = scoreBoardService.GetCurrentFootballMatches().First(i => i.Id == match.Id).TotalScore;
 
             //Assert
+            result.Should().Be(true);
+            expectedTotalScore.Should().BeGreaterThan(match.TotalScore);
         }
     }
 }
